ca5f8c2d83f218c0bc936c1536022e15716f926f
coleYab <yabume123@gmail.com>
2026-03-25 09:53:48 +0000

    fix: rm -rf *

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { UnauthorizedLayout } from "@/components/layouts/UnauthorizedLayout";
-
-export default function Home() {
-  return (
-    <UnauthorizedLayout>
-      <div>This is an authorized layout</div>
-    </UnauthorizedLayout>
-  );
-}
